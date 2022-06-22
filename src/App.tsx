82b6dd865c5aac8c8edb5a3780c4fca9a57c03ef
João Gustavo Bispo <jgbispo20@gmail.com>
2022-06-22 01:06:02 -0300

    welcome v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import {Header} from "./component/Header";
+import {Welcome} from "./component/Welcome";
 
 function App() {
-  return (
-      <div className="flex flex-col min-h-screen">
-          <Header />
-      </div>
-  )
+    return (
+        <div className="flex  items-center flex-col min-h-screen">
+            <Header/>
+            <main className="py-16 items-center">
+                <Welcome/>
+            </main>
+        </div>
+    )
 }
 
 export default App
