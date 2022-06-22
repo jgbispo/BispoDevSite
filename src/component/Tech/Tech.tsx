ef22503bc1954b05f433b8881b9d949bca5e22d3
João Gustavo Bispo <jgbispo20@gmail.com>
2022-06-22 10:29:02 -0300

    New position Works and Techs

--- src/component/Tech/Tech.tsx
+++ src/component/Tech/Tech.tsx
@@ -7,7 +7,7 @@ interface Tech {
 export function Tech(props: Tech) {
     return (
         <div className="flex text-center flex-col gap-1 ml-8 p-4">
-            <img className="rounded-3xl w-[80px] h-[80px]" src={props.url}/>
+            <img className="rounded-2xl w-[60px] h-[60px] " src={props.url}/>
             <p>{props.title}</p>
         </div>
     )
--- src/component/Work/Work.tsx
+++ src/component/Work/Work.tsx
@@ -8,7 +8,7 @@ export function Work(props: Work) {
     return (
         <div className="ml-8 p-4 text-center">
             <a href={props.url} target="_blank" className="flex flex-col gap-3">
-                <img className="rounded-3xl w-[250px] h=[150px]" src={props.urlImg}/>
+                <img className="rounded-3xl w-[250px] h-[150px] bg-center" src={props.urlImg}/>
                 <p>{props.title}</p>
             </a>
         </div>
